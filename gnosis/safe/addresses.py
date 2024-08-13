2c7607208979b8e1314e69ecaabb04fe269288a6
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-08-13 17:32:28 +0300

    feat: add Cronos zkEVM networks

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -869,6 +869,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 657757, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 657761, "1.3.0"),
     ],
+    EthereumNetwork.CRONOS_ZKEVM_MAINNET: [
+        ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 6104, "1.3.0+L2"),
+        ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 6105, "1.3.0"),
+    ],
+    EthereumNetwork.CRONOS_ZKEVM_TESTNET: [
+        ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 163812, "1.3.0+L2"),
+        ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 163814, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1411,5 +1419,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.LISK_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 657735),  # v1.3.0
+    ],
+    EthereumNetwork.CRONOS_ZKEVM_MAINNET: [
+        ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 6097),  # v1.3.0
+    ],
+    EthereumNetwork.CRONOS_ZKEVM_TESTNET: [
+        ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 163805),  # v1.3.0
     ],
 }
